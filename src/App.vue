218892100838ab7e08f648e4f33699b2b47f6efe
MyUsernme <gabriele.morelli@live.it>
2022-02-20 22:34:26 +0100

    esercizio

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MyTitle></MyTitle>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import MyTitle from './components/MyTitle.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    MyTitle
   }
 }
 </script>
